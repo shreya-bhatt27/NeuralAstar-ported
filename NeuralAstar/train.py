ec19b847c941eb85cf04561c1455c297eb0d0fe7
shreya-bhatt27 <emailtoshreya@gmail.com>
2022-01-07 23:35:51 +0530

    updates train parameters

--- NeuralAstar/train.py
+++ NeuralAstar/train.py
@@ -139,7 +139,7 @@ class NeuralAstarModule(pl.LightningModule):
       return optimizer
 
 
-DataModule = AstarDataModule("/home/shreya/Workspaces/mlrc/planning-datasets/data/mpd/all_064_moore_c16.npz")
+DataModule = AstarDataModule("../../planning-datasets/data/mpd/bugtrap_forest_032_moore_c8.npz.npz")
 model = NeuralAstarModule()
-trainer = pl.Trainer()
+trainer = pl.Trainer(gpus=1, num_sanity_val_steps=0, log_every_n_steps=1, max_epochs=15, logger=wandb_logger)
 trainer.fit(model, DataModule)
