24e0b029bd374b43edd5dad3cbb23a25632deefb
Shreya Bhatt <emailtoshreya@gmail.com>
2022-01-15 00:15:42 +0530

    added bbastar

--- NeuralAstar/data_utils/utils/_il_utils.py
+++ NeuralAstar/data_utils/utils/_il_utils.py
@@ -2,8 +2,12 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.utils.data as data
+from __future__ import print_function
+TEST_RANDOM_SEED = 2020
+NUM_POINTS_PER_MAP = 5
 
-def _st_softmax_noexp(val):
+​​def _st_softmax_noexp(val):
     val_ = val.reshape(val.shape[0], -1)
     y = val_ / (val_.sum(dim=-1, keepdim=True))
     _, ind = y.max(dim=-1)
@@ -13,7 +17,6 @@ def _st_softmax_noexp(val):
     y = y.reshape_as(val)
     return (y_hard - y).detach() + y
 
-
 def expand(x, neighbor_filter, padding=1):
     x = x.unsqueeze(0)
     num_samples = x.shape[1]
@@ -21,7 +24,6 @@ def expand(x, neighbor_filter, padding=1):
                  groups=num_samples).squeeze()
     y = y.squeeze(0)
     return y
-
 
 def backtrack(start_maps, goal_maps, parents, current_t):
     num_samples = start_maps.shape[0]
@@ -65,3 +67,59 @@ def dilate_opt_trajs(opt_trajs, map_designs, mechanism):
     ot_conv = torch.clamp(ot_conv.reshape_as(opt_trajs), 0, 1)
     ot_conv = ot_conv * map_designs
     return ot_conv
+
+def get_hard_medium_easy_masks(opt_dists,
+                               device,
+                               reduce_dim: bool = True,
+                               num_points_per_map: int = 5):
+    # make sure the selected nodes are random but fixed
+    #torch.random.seed()
+    # impossible distance
+    wall_dist = torch.min(opt_dists)
+    #print("wall dist:", wall_dist)
+    n_samples = opt_dists.shape[0]
+    #print("n_samples:", n_samples)
+    od_vct = opt_dists.reshape(n_samples, -1)
+    #print("od_vct_size:",od_vct.size())
+    od_nan = od_vct.clone()
+    od_nan[od_nan == wall_dist] = float('nan')
+    od_nan = torch.nan_to_num(od_nan)
+    (od_min, indices) = torch.min(od_nan, axis=1, keepdims=True)
+    #print("od_min:", od_min)
+    #print("od_min_size:", od_min.size())
+    thes = od_min.matmul(torch.tensor([[1.0, 0.85, 0.70, 0.55]], device=device))
+    #print("thes_shape:", thes.size())
+    thes = thes.int()
+    #print("thes_shape:", thes.size())
+    thes = torch.transpose(thes, 0, 1)
+    #print("thes_shape:", thes.size())
+    thes = thes.reshape(4, n_samples, 1, 1, 1)
+    #print("thes_shape:", thes.size())
+​
+    masks_list = []
+    #print("thes:", thes)
+    for i in range(3):
+        binmaps = ((thes[i] <= opt_dists) &
+                   (opt_dists < thes[i + 1])) * 1.0
+        binmaps = torch.repeat_interleave(binmaps, num_points_per_map, 0)
+        masks = _sample_onehot(binmaps, device)
+        masks = masks.reshape(n_samples, num_points_per_map,
+                              *opt_dists.shape[1:])
+        if reduce_dim:
+            (masks, indices) = masks.max(axis=1)
+        masks_list.append(masks.bool())
+    return masks_list
+​
+​
+def _sample_onehot(binmaps, device):
+    n_samples = len(binmaps)
+    binmaps_n = binmaps * torch.rand(binmaps.shape, device= device)
+​
+    binmaps_vct = binmaps_n.reshape(n_samples, -1)
+    ind = binmaps_vct.argmax(axis=-1)
+    onehots = torch.zeros_like(binmaps_vct)
+    onehots[range(n_samples), ind] = 1
+    onehots = onehots.reshape(binmaps_n.shape)
+    onehots = onehots.bool()
+​
+    return onehots
