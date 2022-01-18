4bd880499dbe5cc1f1804bc3fd4d4c3ed7aa9ca7
shreya-bhatt27 <75162393+shreya-bhatt27@users.noreply.github.com>
2022-01-18 12:22:43 +0530

    Update _il_utils.py

--- NeuralAstar/data_utils/utils/_il_utils.py
+++ NeuralAstar/data_utils/utils/_il_utils.py
@@ -81,8 +81,7 @@ def get_hard_medium_easy_masks(opt_dists,
     od_vct = opt_dists.reshape(n_samples, -1)
     od_nan = od_vct.clone()
     #od_nan[od_nan == wall_dist] = float('nan')
-    mask_here = [od_nan == wall_dist]
-    mask_here = torch.tensor(mask_here, device=device)
+    mask_here = torch.tensor([od_nan == wall_dist], device=device)
     mask_here = mask_here.bool()
     od_nan.masked_fill_(mask_here, torch.tensor(float('nan'), device=device))
     od_nan = torch.nan_to_num(od_nan)
