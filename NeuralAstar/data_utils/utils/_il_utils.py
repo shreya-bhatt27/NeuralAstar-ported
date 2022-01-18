bcc0db4b401890ab300af6ad653b4dfadf4852a6
shreya-bhatt27 <75162393+shreya-bhatt27@users.noreply.github.com>
2022-01-18 14:29:59 +0530

    Update _il_utils.py

--- NeuralAstar/data_utils/utils/_il_utils.py
+++ NeuralAstar/data_utils/utils/_il_utils.py
@@ -118,8 +118,11 @@ def _sample_onehot(binmaps, device):
     #od_nan.masked_fill_(mask_here, torch.tensor(float('nan'), device=device))
     #onehots[range(n_samples), ind] = 1
     list_here = [range(n_samples), ind]
-    mask_here = torch.tensor(list_here, device = device)
-    mask_here = torch.stack(mask_here)
+    list_tensors = []
+    for not_a_tensor in list_here:
+        list_tensors.append(torch.tensor(not_a_tensor, device = device))    
+    #mask_here = torch.tensor(list_here, device = device)
+    mask_here = torch.stack(list_tensors)
     mask_here = mask_here.bool().squeeze(0)
     onehots.masked_fill_(mask_here, torch.tensor(1, device=device), device = device)
     onehots = onehots.reshape(binmaps_n.shape)
