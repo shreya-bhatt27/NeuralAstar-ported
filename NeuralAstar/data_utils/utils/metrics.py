3c8118add5f547c8ccaf89ac9c12b0a39a48f92a
Aayush Jain <77500156+Aayush-Jain01@users.noreply.github.com>
2022-01-27 15:20:23 +0530

    Update metrics.py

--- NeuralAstar/data_utils/utils/metrics.py
+++ NeuralAstar/data_utils/utils/metrics.py
@@ -1,6 +1,6 @@
 import torch
 
-def compute_opt_suc_exp(pred_dists, rel_exps, opt_dists, masks):
+def compute_opt_suc_exp(pred_dists, rel_exps, pred_exps, exps, opt_dists, masks):
 
     wall_dist = torch.min(opt_dists)  # impossible distance
     diff_dists = pred_dists - opt_dists
@@ -8,17 +8,24 @@ def compute_opt_suc_exp(pred_dists, rel_exps, opt_dists, masks):
     suc = ~(pred_dists == wall_dist)[masks]
     exp = (torch.ones_like(pred_dists)[masks] if rel_exps is None else
            rel_exps[(pred_dists != wall_dist) & masks])
+    num = (torch.ones_like(pred_dists)[masks] if  pred_exps is None else
+           pred_exps[(pred_dists != wall_dist) & masks])
+    denom = (torch.ones_like(pred_dists)[masks] if exps is None else
+           exps[(pred_dists != wall_dist) & masks])
     if len(exp) == 0:
         exp = torch.ones_like(pred_dists)[masks]  ## workaround
+    return opt, suc, exp, num, denom
 
-    return opt, suc, exp
 
+def compute_mean_metrics(pred_dists, rel_exps, pred_exps, exps, opt_dists, masks):
 
-def compute_mean_metrics(pred_dists, rel_exps, opt_dists, masks):
-
-    opt, suc, exp = compute_opt_suc_exp(pred_dists, rel_exps, opt_dists, masks)
-
-    return opt.float().mean(), suc.float().mean(), exp.float().mean()
+    opt, suc, exp, num, denom = compute_opt_suc_exp(pred_dists, rel_exps, pred_exps, exps, opt_dists, masks)
+#     exp_length = len(exp)
+#     print("Opt Length =", len(opt))
+    print("Exp Length =", len(exp))
+    print("Num Length =", len(num))
+    print("Denom Length =", len(denom))
+    return opt.float().mean(), suc.float().mean(), exp.float().mean(), num.float().mean(), denom.float().mean()
 
 
 def compute_opt_exp(pred_dists, rel_exps, opt_dists, masks):
