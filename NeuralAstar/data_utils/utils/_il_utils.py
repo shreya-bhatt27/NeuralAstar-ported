ec44bdccd54fda73f232c8550b3865e93f90b35b
shreya-bhatt27 <75162393+shreya-bhatt27@users.noreply.github.com>
2022-02-02 17:25:56 +0530

    Update _il_utils.py

--- NeuralAstar/data_utils/utils/_il_utils.py
+++ NeuralAstar/data_utils/utils/_il_utils.py
@@ -24,8 +24,7 @@ def _st_softmax_noexp(val):
 def expand(x, neighbor_filter, padding=1):
     x = x.unsqueeze(0)
     num_samples = x.shape[1]
-    y = F.conv2d(x.double(), neighbor_filter, padding=padding,
-                 groups=num_samples).squeeze()
+    y = F.conv2d(x.double(), neighbor_filter.double(), padding=padding, groups=num_samples).squeeze()
     y = y.squeeze(0)
     return y
 
