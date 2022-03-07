c87babbebeb582df9b6e1baff1879ec19ae7c72e
Aayush Jain <77500156+Aayush-Jain01@users.noreply.github.com>
2022-03-08 03:11:54 +0530

    Update rundropout.py

--- Dropout/NeuralAstar/rundropout.py
+++ Dropout/NeuralAstar/rundropout.py
@@ -8,12 +8,12 @@ from pytorch_lightning.loggers import WandbLogger
 import wandb
 import random
 
-def main(whether_wandb, wandb_login):
+def main():
     print("HELLOOO FIRST")
     logger = None
-    if whether_wandb == True:
-        subprocess.call('wandb login '  + str(wandb_login))
-        logger = WandbLogger(project="Neural-astar-dropout-experiment")
+#     if whether_wandb == True:
+#         subprocess.call('wandb login '  + str(wandb_login))
+#         logger = WandbLogger(project="Neural-astar-dropout-experiment")
     subprocess.call('cd ../')
     subprocess.call('mkdir checkpoints')
     subprocess.call('cd NeuralAstar')
@@ -25,23 +25,23 @@ def main(whether_wandb, wandb_login):
          mode = 'max',
     )
     pl.utilities.seed.seed_everything(1993, workers = True)
-    print("HELLLLLOOOO_BEFORE")
+#     print("HELLLLLOOOO_BEFORE")
     DataModule = AstarDataModule("../../data/mpd/multiple_bugtraps_032_moore_c8.npz")
     model = NeuralAstarModule(0.0, 'vgg16_bn', True, 'm+')
     trainer = pl.Trainer(gpus=1, log_every_n_steps=1,callbacks=[checkpoint_callback], max_epochs=400,logger=logger,num_sanity_val_steps=0,gradient_clip_val=40,weights_summary="full", deterministic=True)
     trainer.fit(model, DataModule)
-    print("HELLLLLOOOO_AFTER")
+#     print("HELLLLLOOOO_AFTER")
     trainer.save_checkpoint("dropout.pth")
     if whether_wandb:
         wandb.save("dropout.pth")
     trainer.test(model, DataModule.test_dataloader())
-    print("HELLO_LAST")
+#     print("HELLO_LAST")
 
 if __name__ == '__main__':
     main()
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--whether_wandb', metavar='path', required=True)
-    parser.add_argument('--wandb_login', metavar='path', required=True)
-    args = parser.parse_args()
-    main(workspace=args.workspace)
+#     import argparse
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--whether_wandb', metavar='path', required=True)
+#     parser.add_argument('--wandb_login', metavar='path', required=True)
+#     args = parser.parse_args()
+#     main(workspace=args.workspace)
