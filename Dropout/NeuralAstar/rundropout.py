9e1103c7f2d38883c3909c921fa9157cd60a6682
Aayush Jain <77500156+Aayush-Jain01@users.noreply.github.com>
2022-03-08 03:14:16 +0530

    Update rundropout.py

--- Dropout/NeuralAstar/rundropout.py
+++ Dropout/NeuralAstar/rundropout.py
@@ -14,21 +14,21 @@ def main():
 #     if whether_wandb == True:
 #         subprocess.call('wandb login '  + str(wandb_login))
 #         logger = WandbLogger(project="Neural-astar-dropout-experiment")
-    subprocess.call('cd ../')
-    subprocess.call('mkdir checkpoints')
-    subprocess.call('cd NeuralAstar')
-    checkpoint_callback = ModelCheckpoint(
-         dirpath='../checkpoints',
-         filename='{epoch}-{hmean:.2f}-{los_tot:.2f}',
-         monitor='hmean',
-         save_top_k = -1,
-         mode = 'max',
-    )
+#     subprocess.call('cd ../')
+#     subprocess.call('mkdir checkpoints')
+#     subprocess.call('cd NeuralAstar')
+#     checkpoint_callback = ModelCheckpoint(
+#          dirpath='../checkpoints',
+#          filename='{epoch}-{hmean:.2f}-{los_tot:.2f}',
+#          monitor='hmean',
+#          save_top_k = -1,
+#          mode = 'max',
+#     )
     pl.utilities.seed.seed_everything(1993, workers = True)
 #     print("HELLLLLOOOO_BEFORE")
     DataModule = AstarDataModule("../../data/mpd/multiple_bugtraps_032_moore_c8.npz")
     model = NeuralAstarModule(0.0, 'vgg16_bn', True, 'm+')
-    trainer = pl.Trainer(gpus=1, log_every_n_steps=1,callbacks=[checkpoint_callback], max_epochs=400,logger=logger,num_sanity_val_steps=0,gradient_clip_val=40,weights_summary="full", deterministic=True)
+    trainer = pl.Trainer(gpus=1, log_every_n_steps=1, max_epochs=400,logger=logger,num_sanity_val_steps=0,gradient_clip_val=40,weights_summary="full", deterministic=True)
     trainer.fit(model, DataModule)
 #     print("HELLLLLOOOO_AFTER")
     trainer.save_checkpoint("dropout.pth")
